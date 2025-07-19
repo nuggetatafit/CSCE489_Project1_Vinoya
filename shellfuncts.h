28b3e94ecf69c05992929796223670f2eec0f2ad
nuggetatafit <nathaniel.vinoya.1@us.af.mil>
2025-07-18 23:44:27 -0400

    Completed Project #1

--- shellfuncts.h
+++ shellfuncts.h
@@ -1,9 +1,26 @@
 /*************************************************************************************
- * shellfuncts.h - header file for your shell functions
+ * shellfuncts.h - header file for shell functions
  *
  *************************************************************************************/
 
-// Replace these with something useful
-int hello(int param1);
-void send_msg(const char *param2);
+#ifndef SHELLFUNCTS_H
+#define SHELLFUNCTS_H
 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+void cmd_loop(void);
+void trim_whitespace(char *str);
+void cmd_create(char *filename);
+void cmd_update(char *filename, int count, char *text);
+void cmd_list(char *filename);
+void cmd_dir(void);
+
+#endif
